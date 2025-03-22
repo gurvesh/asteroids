39a0782518f8c490be20c432379b10e01a28b62f
gurvesh <guru01@gmail.com>
2025-03-22 23:31:58 +0530

    C: finished player and added containers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -9,16 +10,34 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Adding pygame groups
+    # All the objects that can be updated:
+    updatable = pygame.sprite.Group()
+    # All the objects that can be drawn:
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     clock = pygame.time.Clock()
     dt = 0
-
+    player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill(pygame.Color(0,0,0))
+        screen.fill("black")
+        # player.update(dt)
+        # Using the updatable group container now:
+        for thing in updatable:
+            thing.update(dt)
+        
+        # player.draw(screen)
+        # Using the drawable group container:
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
